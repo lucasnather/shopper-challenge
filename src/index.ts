881f0990c83d100269654bec28aff807f48fd7f8
lucasnather <natherlucas@gmail.com>
2024-08-27 15:59:31 -0400

    feat: server running

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 8080
+
+app.get('/', async (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Application running at port http://localhost:${port}/`)
+})
