93c190d240ccad74d76b0441ed7a659426aea798
lucasnather <natherlucas@gmail.com>
2024-08-28 09:07:47 -0400

    feat: add repository layer with create and findByMonth

--- src/application/gateway/GeminiFactory.ts
+++ src/application/gateway/GeminiFactory.ts
@@ -1,7 +1,8 @@
 import { Prisma } from "@prisma/client";
 import { Consumption } from "../../domain/Consumption.js";
+import { MeasureType } from "../../domain/enum/MeasureType.js";
 
 export interface GeminiFactory {
     create(data: Prisma.ComsumptionsCreateInput): Promise<Consumption>
-    findByMonth(measureDatetime: Date): Promise<Consumption | null>
+    findByMonth(measureType: MeasureType, measureDatetime: Date): Promise<Consumption | null>
 }
--- src/infra/repository/GeminiPrismaRepository.ts
+++ src/infra/repository/GeminiPrismaRepository.ts
@@ -1,15 +1,51 @@
 import { Prisma } from "@prisma/client";
 import { GeminiFactory } from "../../application/gateway/GeminiFactory.js";
 import { Consumption } from "../../domain/Consumption.js";
+import { prisma } from "../../config/prisma.js";
+import { GeminiMapper } from "../gateway/GeminiMapper.js";
+import { MeasureType } from "../../domain/enum/MeasureType.js";
 
 export class GeminiPrismaRepository implements GeminiFactory {
 
+    constructor(private geminiMapper: GeminiMapper) {}
+
     async create(data: Prisma.ComsumptionsCreateInput): Promise<Consumption> {
+        const consumption = await prisma.comsumptions.create({
+            data: {
+                customerCode: data.customerCode,
+                measureValue: data.measureValue,
+                measureDatetime: data.measureDatetime,
+                measureType: data.measureType,
+                imageUrl: data.imageUrl
+            }
+        })
+
+        const consumptionToDomain = this.geminiMapper.toDomain(consumption);
+
+        return consumptionToDomain
         
     }
 
-    async findByMonth(measureDatetime: Date): Promise<Consumption | null> {
-        throw new Error("Method not implemented.");
+    async findByMonth(measureType: MeasureType, measureDatetime: Date): Promise<Consumption | null> {
+        const month = measureDatetime.getMonth() + 1
+
+        const consumption = await prisma.comsumptions.findFirst({
+            where: {
+                measureType,
+                measureDatetime
+            }
+        })
+
+        if(!consumption) return null
+
+        const consumptionMonth = consumption.measureDatetime.getMonth() + 1
+
+        if(month === consumptionMonth) return null
+
+        const consumptionToDomain = this.geminiMapper.toDomain(consumption)
+
+        return consumptionToDomain
+
     }
 
 }
