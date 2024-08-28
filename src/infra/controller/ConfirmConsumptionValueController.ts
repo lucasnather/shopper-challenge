9a524eb1fc7ef99a6498adc25421a9acc4e9d1f5
lucasnather <natherlucas@gmail.com>
2024-08-28 11:11:21 -0400

    feat: implement controller confirm consumption by value

---
+++ src/infra/controller/ConfirmConsumptionValueController.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import type { Request, Response } from 'express'
+import { GeminiPrismaRepository } from "../repository/GeminiPrismaRepository.js";
+import { GeminiMapper } from "../gateway/GeminiMapper.js";
+import { ConfirmConsumptionValueService } from "../../application/services/ConfirmConsumptionValue.js";
+
+const confirmConsumptionValueBodySchema = z.object({
+    measure_id: z.string().uuid(),
+    measure_value: z.coerce.number()
+})
+
+export class ConfirmConsumptionValueController {
+
+   async post(req: Request, res: Response) {
+    const { measure_id: measureId, measure_value: measureValue  } = confirmConsumptionValueBodySchema.parse(req.body)
+
+    const geminiMapper = new GeminiMapper()
+    const geminiPrismaRepository = new GeminiPrismaRepository(geminiMapper)
+    const confirmConsumptionValueService = new ConfirmConsumptionValueService(geminiPrismaRepository)
+
+    try {
+        const { success } = await confirmConsumptionValueService.execute({
+            measureId,
+            measureValue
+        })
+
+        return res.status(201).json({
+            success
+        })
+    } catch(e) {
+        return res.status(404).send({
+            message: "Erro por enquanto"
+        })
+    }
+   }
+}
