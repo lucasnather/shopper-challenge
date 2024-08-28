a75b5a9e34f27bec3ed73e942d77fe5c3a65c1da
lucasnather <natherlucas@gmail.com>
2024-08-28 11:13:46 -0400

    feat: add route /confirm

--- src/infra/routes/gemini-routes.ts
+++ src/infra/routes/gemini-routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { CreateConsumptionController } from '../controller/CreateConsumptionController.js'
+import { ConfirmConsumptionValueController } from '../controller/ConfirmConsumptionValueController.js'
 
 const router = Router()
 
 const createGeminiController = new CreateConsumptionController()
+const confirmConsumptionValueController = new ConfirmConsumptionValueController()
 
 router 
     .post('/upload', (req, res) => createGeminiController.upload(req, res))
+    .patch('/confirm', (req, res) => confirmConsumptionValueController.confirm(req, res))
     
 export const geminRouter = router
