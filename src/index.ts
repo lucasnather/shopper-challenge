65074a40518f9194db41478c69cbb930ef3b913b
lucasnather <natherlucas@gmail.com>
2024-08-28 08:30:44 -0400

    fix: remove route test /upload

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-import express, {Request, Response} from 'express'
-import { z } from 'zod'
-import { GeminiImageAnalyze } from './domain/gemini/GeminiImageAnalyze.js';
-import { Converter } from './domain/utils/Converter.js';
+import express from 'express'
 
 const app = express()
 
@@ -13,23 +10,6 @@ app.get('/', async (req, res) => {
     res.json({
         message: "Hello World"
     })
-})
-
-const bodySchema = z.object({
-    image: z.string()
-})
-
-app.post('/upload', async (req: Request, res: Response) => {
-   const { image } = bodySchema.parse(req.body)
-
-   const converter = new Converter()
-   converter.convertToPNG(image)
-
-    const gemini = new GeminiImageAnalyze()
-    const geminiResponse = await gemini.response('./image.png')
-    console.log(geminiResponse)
-
-   res.send({})
 })
 
 app.listen(port, () => {
